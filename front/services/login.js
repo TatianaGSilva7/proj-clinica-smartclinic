89763f54f81824d9cf865b578ba6fbdadae56733
VictorLemosbar <victor.escolas9045@gmail.com>
2026-08-05 19:36:11 -0300

    Adicionado repositório do front e pastas services

---
+++ front/services/login.js
@@ -0,0 +1,13 @@
+export const login = async (user) => {
+  const response = await fetch("http://localhost:8080", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"//,
+      //Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify(data)
+  });
+
+  const data = await response()
+  return data
+};
