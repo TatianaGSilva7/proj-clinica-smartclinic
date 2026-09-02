71308dab814413e3b0176813b77c5608b28187a8
VictorLemosbar <victor.escolas9045@gmail.com>
2026-09-02 20:52:21 -0300

    Feat - Adicionado atualizarMedico

--- src/domains/medicos/services/medicosService.js
+++ src/domains/medicos/services/medicosService.js
@@ -1,9 +1,23 @@
-import { BASE_URL } from '../../../api/config';
+import { BASE_URL } from "../../../api/config";
 
 export async function buscarMedicos() {
   const resposta = await fetch(`${BASE_URL}/medicos`);
   if (!resposta.ok) {
     throw new Error(`Erro HTTP ${resposta.status}`);
   }
+  return await resposta.json();
+}
+
+export async function atualizarMedico(id, dados) {
+  const resposta = await fetch(`${BASE_URL}/medicos/${id}`, {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(dados),
+  });
+
+  if (!resposta.ok) {
+    throw new Error(`Erro HTTP ${resposta.status} ao atualizar`);
+  }
+
   return await resposta.json();
 }
