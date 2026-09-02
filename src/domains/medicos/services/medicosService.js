3d13b102bbf95ee4aa2032b1fce6a8d4dd2bb6fa
TatianaGSilva7 <tatiggarcia@gmail.com>
2026-09-02 20:49:12 -0300

    feat: adicionado metodo para excluir médico

--- src/domains/medicos/services/medicosService.js
+++ src/domains/medicos/services/medicosService.js
@@ -7,3 +7,13 @@ export async function buscarMedicos() {
   }
   return await resposta.json();
 }
+
+ const excluirMedico = async (id) => {
+    const resposta = await fetch(`${BASE_URL}/medicos/${id}`, {
+      method: 'DELETE',
+    });
+
+    if (!resposta.ok) {
+      throw new Error(`Erro HTTP ${resposta.status} ao excluir`);
+    }
+  }
